0664badd9e15a43309673423cf0118159f6f4490
madan3242 <madangowda3242@gmail.com>
2023-03-31 14:20:30 +0530

    client updated  #3

---
+++ client/src/pages/productPage/ProductList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Alert } from 'bootstrap'
+
+export const ProductList = () => {
+  return (
+    <>
+      <div className="container">
+        hi
+      </div>
+    </>
+  )
+}
