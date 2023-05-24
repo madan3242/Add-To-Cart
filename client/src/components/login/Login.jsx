55f11da668daf33ed5e7817ea1ce34e3c013faa2
madan3242 <madangowda3242@gmail.com>
2023-05-24 17:52:47 +0530

    footer list updated

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,26 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
-import { FloatingLabel, Form } from "react-bootstrap";
-import { GrClose } from 'react-icons/gr'
+import { Button, FloatingLabel, Form } from "react-bootstrap";
+import { GrClose } from "react-icons/gr";
 
-const Login = ({toggleLogin}) => {
+const Login = ({ toggleLogin }) => {
+  const [loginUserData, setLoginUserData] = useState({
+    email: "",
+    password: "",
+  });
+  const [signupUserData, setSignupUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [isLogin, setIsLogin] = useState(true);
+
   return (
-    <div className="login-form">
-        <div className="form">
-            <GrClose size={30} className="close-icon" onClick={toggleLogin} />
-        </div>
-      <h2 className="mb-4">Login</h2>
-      <FloatingLabel
-        controlId="floatingInput"
-        label="Email address"
-        className="mb-3"
-      >
-        <Form.Control type="email" placeholder="name@example.com" />
-      </FloatingLabel>
-      <FloatingLabel controlId="floatingPassword" label="Password">
-        <Form.Control type="password" placeholder="Password" />
-      </FloatingLabel>
-    </div>
+    <>
+      <div className="login-form">
+        <GrClose size={30} className="close-icon" onClick={toggleLogin} />
+        {isLogin ? (
+          <>
+            <h2 className="mb-4">Login</h2>
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Email address"
+              className="mb-3"
+            >
+              <Form.Control type="email" placeholder="name@example.com" />
+            </FloatingLabel>
+            <FloatingLabel
+              controlId="floatingPassword"
+              label="Password"
+              className="mb-3"
+            >
+              <Form.Control type="password" placeholder="Password" />
+            </FloatingLabel>
+
+            <div className="text-center">
+              <Button variant="success" className="mb-2">
+                Login
+              </Button>
+              <p>
+                Don't have an account ?{" "}
+                <span
+                  className="span-button"
+                  onClick={() => setIsLogin(!isLogin)}
+                >
+                  Sign up
+                </span>{" "}
+              </p>
+            </div>
+          </>
+        ) : (
+          <>
+            <h2 className="mb-4">Signup</h2>
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Email address"
+              className="mb-3"
+            >
+              <Form.Control type="email" placeholder="name@example.com" />
+            </FloatingLabel>
+
+            <FloatingLabel
+              controlId="floatingPassword"
+              label="Password"
+              className="mb-3"
+            >
+              <Form.Control type="password" placeholder="Password" />
+            </FloatingLabel>
+
+            <FloatingLabel
+              controlId="floatingConfirmPassword"
+              label="Confirm Password"
+              className="mb-3"
+            >
+              <Form.Control type="password" placeholder="Confirm Password" />
+            </FloatingLabel>
+
+            <div className="text-center">
+              <Button variant="success" className="mb-2">
+                Signup
+              </Button>
+              <p>
+                Already have an account ?{" "}
+                <span
+                  className="span-button"
+                  onClick={() => setIsLogin(!isLogin)}
+                >
+                  Login
+                </span>
+              </p>
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -3,6 +3,9 @@ import { useNavigate } from "react-router-dom";
 import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
 import "./Navbar.css";
 
+import { AiOutlineHeart } from 'react-icons/ai'
+import { BsCart2 } from 'react-icons/bs'
+
 const HomeNavbar = ({toggleDropdown, toggleLogin}) => {
   const navigate = useNavigate();
   return (
@@ -27,8 +30,13 @@ const HomeNavbar = ({toggleDropdown, toggleLogin}) => {
               aria-label="Search"
             />
           </Form>
-          <Button onClick={toggleLogin} variant="null">Login / SignUp</Button>
-          </Container>
+          <div className="navbar-buttons">
+            <Button onClick={toggleLogin} variant="null">Login / SignUp</Button>
+
+            <AiOutlineHeart size={20} />
+            <BsCart2 size={20} />
+          </div>
+        </Container>
       </Navbar>
     </>
   );
