79669c67a66e9f54b50ac296ca14a58574fc62fc
madan3242 <madangowda3242@gmail.com>
2023-05-30 12:22:29 +0530

    product add controller updated

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,56 +1,21 @@
 const express = require("express");
-const cloudinary = require("cloudinary").v2;
-const Product = require("../models/product");
 const { 
     getAllProducts, 
     deleteProductById, 
-    getSingleProduct 
+    getSingleProduct, 
+    adminAddProduct
 } = require("../controllers/productController");
+const { 
+    isLoggedIn, 
+    customRole 
+} = require('../middlewares/user');
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/products/:id").get(getSingleProduct);
 
-
-router.route("/admin/products/add").post(async (req, res) => {
-  try {
-    let imageArray = [];
-
-    if(!req.files) {
-        return res.status(401).json({
-            message: "Product images are required",
-          });
-    }
-
-    if(req.files){
-        for(i = 0; i < req.files.photos; i++){
-            let result = await cloudinary.uploader.upload(req.files.photos[i].tempFilePath, {
-                folder: "products"
-            })
-
-            imageArray.push({
-                id: result.public_id,
-                secure_url: result.secure_url
-            })
-        }
-    }
-
-    req.body.photos = imageArray;
-    req.body.user = req.user.id;
-
-    const product = await Product.create(req.body);
-
-    res.status(201).json({
-        msg: "Product created successfully",
-        product
-    })
-  } catch (error) {
-    res.status(500).json({
-        error: `Your request cannot be processed. Please try again`
-    })
-  }
-});
+router.route("/admin/products/add").post(isLoggedIn, customRole('admin') ,adminAddProduct);
 
 router.route("products/:id").delete(deleteProductById);
 
