f56629c33c321a7dfafa62a0b5d5d8bf4b74299f
madan3242 <madangowda3242@gmail.com>
2023-06-13 18:01:14 +0530

    init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/homePage/Home'
+import { setAuthToken } from './services/setAuthToken'
+
+if(localStorage.token){
+  setAuthToken(localStorage.token)
+}
 
 const App = () => {
   return (
