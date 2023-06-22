6abbc20892c4c84105f9b87b88298e3a28f9ebba
madan3242 <madangowda3242@gmail.com>
2023-06-22 17:03:13 +0530

    init

--- server/utils/emailHelper.js
+++ server/utils/emailHelper.js
@@ -1,5 +1,25 @@
 const nodemailer = require('nodemailer')
 
-exports.sendOtp = async () => {
+exports.sendOtp = async (options) => {
+    // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.SMTP_USER, // generated ethereal user
+      pass: process.env.SMTP_PASS, // generated ethereal password
+    },
+  });
 
+  let message = {
+    from: '"Add To Cart" <madangowda323@gmail.com>', // sender address
+    to: options.email, // list of receivers
+    subject: options.subject, // Subject line
+    text: options.text, // plain text body
+  };
+
+  await transporter.sendMail(message)  
 }
+
+module.exports = sendOtp
