3f104f4841c5c7f3e4df4eab922534287fb06324
madan3242 <madangowda3242@gmail.com>
2023-07-21 17:44:17 +0530

    admin

--- client/src/components/Admin/components/Products/Products.jsx
+++ client/src/components/Admin/components/Products/Products.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
-import { Button, Col, Row } from 'react-bootstrap'
+import { Button, Col, Row, Table } from 'react-bootstrap'
 import AddProduct from './AddProduct'
 import './product.css'
+import { useSelector } from 'react-redux'
 
 const Products = () => {
   const [addProduct, setAddProduct] = useState(false)
+  const products = useSelector((state) => state.products);
 
   const toggleAddProduct = () => {
     setAddProduct(!addProduct)
@@ -18,8 +20,39 @@ const Products = () => {
         <Row>
             <h2>Products</h2>
         </Row>
-        <Row>
-          
+        <Row style={{ margin: "1rem"}}>
+          <Table striped bordered hover variant="secondary">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Description</th>
+                <th>Photo</th>
+                <th>Category</th>
+                <th>Brand</th>
+                <th>Stocks</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                products?.length > 0 ? <>{
+                  products.map((product) => {
+                    return <tr>
+                      <td>{product.id}</td>
+                      <td>{product.name}</td>
+                      <td>{product.price}</td>
+                      <td>{product.description}</td>
+                      <td>{product.photo[0]}</td>
+                      <td>{product.category}</td>
+                      <td>{product.brand}</td>
+                      <td>{product.stocks}</td>
+                    </tr>
+                  })
+                }</> : null
+              }
+            </tbody>
+          </Table>
         </Row>
 
       </div>
--- client/src/components/Admin/components/Users/User.jsx
+++ client/src/components/Admin/components/Users/User.jsx
@@ -1,42 +1,63 @@
 import React from "react";
 import { Row, Table } from "react-bootstrap";
-import { AiOutlineDelete } from "react-icons/ai";
+import { AiOutlineDelete, AiOutlineEdit } from "react-icons/ai";
+import { useSelector } from "react-redux";
 
 const User = () => {
+  const users = useSelector((state) => state.users);
   return (
     <>
       <Row>
         <h2>Users</h2>
       </Row>
       <Row style={{ margin: "1rem"}}>
-        <Table striped bordered hover style={{ width: "100%" }}>
-          <thead>
-            <tr>
+      <Table striped bordered hover variant="secondary">
+            <thead>
+              <tr>
               <th>ID</th>
               <th>Name</th>
               <th>Email</th>
               <th>Phone</th>
               <th>Role</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-              <td>@mdo</td>
-              <td><AiOutlineDelete size={20} /></td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-          </tbody>
-        </Table>
+              <th>Update/Delete</th>
+              <th>Update/Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Test</td>
+                <td>Test</td>
+                <td>Test</td>
+                <td>Test</td>
+                <td>Test</td>
+                <td>
+                  <AiOutlineEdit size={20} />
+                  {/* <AiOutlineDelete size={20} /> */}
+                </td>
+                <td>
+                  {/* <AiOutlineEdit size={20} /> */}
+                  <AiOutlineDelete size={20} />
+                </td>
+              </tr>
+              {
+                users?.length > 0 ? <>{
+                  users.map((product) => {
+                    return <tr>
+                      <td>{users.id}</td>
+                      <td>{users.name}</td>
+                      <td>{users.email}</td>
+                      <td>{users.phone}</td>
+                      <td>{users.role}</td>
+                      <td>
+                        <AiOutlineEdit size={20} />
+                        <AiOutlineDelete size={20} />
+                      </td>
+                    </tr>
+                  })
+                }</> : null
+              }
+            </tbody>
+          </Table>
       </Row>
     </>
   );
