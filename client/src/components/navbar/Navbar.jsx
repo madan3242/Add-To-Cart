b9726c33c0cc4a29a6e7c5d2bf3dfe88d21c4be1
madan3242 <madangowda3242@gmail.com>
2023-07-24 17:37:07 +0530

    navbar updated

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -13,54 +13,58 @@ const HomeNavbar = ({ isAuthenticated, setIsAuthenticated }) => {
   
   return (
     <>
-      <Navbar className="navbar" fixed="top">
+      <Navbar className="navbar" fixed="top" expand="lg">
         <Container>
           <Navbar.Brand onClick={() => navigate('/')} style={{ cursor: "pointer"}}>
               Add To Cart
           </Navbar.Brand>
 
-          {/* <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
           <Navbar.Offcanvas
-            id={`offcanvasNavbar-expand-lg`}
-            aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
-            placement="end"
+              id={`offcanvasNavbar-expand-lg`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+              placement="end"
           >
             <Offcanvas.Header closeButton>
-              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
-                Offcanvas
-              </Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
+                  Add To Cart
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link onClick={() => navigate('/products')}>Products</Nav.Link>
+                  <Nav.Link onClick={() => navigate('/products')}>Mobiles</Nav.Link>
+                  <Nav.Link >Electronics</Nav.Link>
+                  <Nav.Link >Fashion</Nav.Link>
+                  <Form>
+                    <Form.Control
+                      type="search"
+                      placeholder="Search for products"
+                      className="me-2"
+                      aria-label="Search"
+                      style={{ width: "300px"}}
+                    />
+                  </Form>
+                  <div style={{ display: "flex", placeItems: "center", padding: "0 15px"}}>
+                    {
+                      !isAuthenticated ? <>
+                        <Button onClick={() => navigate('/login')} variant="null">Login / Signup</Button>
+                      </> : <>
+                        <Button variant="null" onClick={() => dispatch(logout(setIsAuthenticated))} >Logout</Button>
+                        <AiOutlineUser size={25} onClick={() => navigate('/profile')} style={{ margin: "0 10px"}} />
+                      </>
+                    }
+                    <AiOutlineHeart size={25} onClick={() => navigate('/wishlist')} style={{ margin: "0 10px"}} />
+                    <BsCart2 size={25} onClick={() => navigate('/cart')} style={{ margin: "0 1px"}} />
+                  </div>
+                </Nav>
+              </Offcanvas.Body>
+          </Navbar.Offcanvas>
 
-            </Offcanvas.Body>
-          </Navbar.Offcanvas> */}
+          
 
-          <div className="d-flex">
-            <Nav.Link onClick={() => navigate('/products')}>Mobiles</Nav.Link>
-            <Nav.Link >Electronics</Nav.Link>
-            <Nav.Link >Fashion</Nav.Link>
-          </div>
-
-          <Form>
-            <Form.Control
-              type="search"
-              placeholder="Search for products"
-              className="me-2"
-              aria-label="Search"
-            />
-          </Form>
-          <div className="navbar-buttons">
-            {
-              !isAuthenticated ? <>
-                <Button onClick={() => navigate('/login')} variant="null">Login / Signup</Button>
-              </> : <>
-                <Button variant="null" onClick={() => dispatch(logout(setIsAuthenticated))}>Logout</Button>
-                <AiOutlineUser size={20} onClick={() => navigate('/profile')} />
-              </>
-            }
-            <AiOutlineHeart size={20} onClick={() => navigate('/wishlist')} />
-            <BsCart2 size={20} onClick={() => navigate('/cart')} />
-          </div>
+          
+          
         </Container>
       </Navbar>
     </>
