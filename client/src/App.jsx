b107c74a88bf9570d9557817aadcb2be993d56c2
Madan <madangowda3242@gmail.com>
2024-01-10 09:31:13 +0530

    Stripe fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,30 @@
 import React, { Suspense, useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { Toaster } from 'react-hot-toast'
+import { Spinner } from 'react-bootstrap'
+
 import Admin from './components/Admin/Admin'
 import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
-import Profile from './components/Profile/Profile'
+import Footer from './components/Footer/Footer'
+
 import Products from './components/Products/Products'
-import Login from './components/Login/Login'
-import { setAuthToken } from './services/setAuthToken'
-import NotFound from './components/NotFound/NotFound'
-import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
-import { useSelector } from 'react-redux'
 import Product from './components/Products/Product'
-import ForgotPassword from './components/Login/ForgotPassword'
-import ResetPassword from './components/Login/ResetPassword'
 import Cart from './components/Cart/Cart'
 import Shipping from './components/Cart/Shipping'
 import ConfirmOrder from './components/Cart/ConfirmOrder'
 import Payment from './components/Cart/Payment'
-import { Toaster } from 'react-hot-toast'
 import OrderSuccess from './components/Cart/OrderSuccess'
-import { Spinner } from 'react-bootstrap'
+
+import Login from './components/Login/Login'
+import ForgotPassword from './components/Login/ForgotPassword'
+import ResetPassword from './components/Login/ResetPassword'
+import Profile from './components/Profile/Profile'
+
+import { setAuthToken } from './services/setAuthToken'
+import NotFound from './components/NotFound/NotFound'
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
 
 const App = () => {
   const { user } = useSelector(state => state.auth);
@@ -39,7 +43,6 @@ const App = () => {
         <Suspense 
           fallback={<Spinner />}
         >
-
           <Navbar isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
           <Routes>
               <Route path='/process/payment' element={
@@ -48,7 +51,6 @@ const App = () => {
                 </ProtectedRoute>
               } />
             
-
             <Route path='/' exact element={<Home />}  />
             <Route path='/login' element={ <Login />} />
             <Route path='/forgotpassword' element={<ForgotPassword />} />
@@ -82,12 +84,14 @@ const App = () => {
               </ProtectedRoute>
             } />
 
+            {/* Admin Routes */}
             <Route path='/admin/*' element={
               <ProtectedRoute user={user} isAdmin={true}>
                 <Admin />
               </ProtectedRoute>
             } />
 
+            {/* Route not found */}
             <Route element={window.location.pathname === '/process/payment' ? null : <NotFound /> }  />
             <Route path='*' element={<NotFound />} />
           </Routes>
