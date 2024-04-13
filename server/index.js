7f58f65389f99b7569d3bb228486f9ba00106c55
Madan <madangowda3242@gmail.com>
2024-04-13 12:51:03 +0530

    new admin dash

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app = require('./src/app');
-const connectDb = require('./src/config/dbConfig');
+const connectDb = require('./src/config');
 const cloudinary = require('cloudinary').v2;
 
 /**
@@ -9,9 +9,10 @@ cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET
-})
+});
+
+connectDb();
 
 app.listen(process.env.PORT, ()=> {
-    connectDb();
     console.log(`SERVER RUNNING ON PORT ${process.env.PORT}`);
-})
+});
--- server/src/config/index.js
+++ server/src/config/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDb = () => {
     mongoose
         .connect(process.env.DB_URL)
-        .then(console.log(`MONGODB CONNECTED SUCCESSFULLY`))
+        .then(console.log(`DATABASE CONNECTED SUCCESSFULLY`))
         .catch(error => {
             console.log(`DB CONNECTION FAILED`);
             console.log(error);
