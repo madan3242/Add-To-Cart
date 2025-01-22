d6d46000de7ec2ae89798a33580f2a76d8920a01
Madan <madangowda3242@gmail.com>
2025-01-22 14:13:05 +0530

    admin deleted

--- admin/src/config/index.js
+++
@@ -1,7 +0,0 @@
-export const API_URL = `${import.meta.env.VITE_API_URI}`;
-
-export const config = {
-  headers: {
-    "Content-Type": "application/json",
-  },
-};
--- admin/src/services/setAuthToken.js
+++
@@ -1,9 +0,0 @@
-import Axios from "axios"
-
-export const setAuthToken = (token) => {
-    if(token){
-        Axios.defaults.headers.common["Authorization"] = `Bearer ${token}`
-    } else {
-        delete Axios.defaults.headers.common["Authorization"];
-    }
-}
